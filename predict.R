53eb8afa7d40a79e77852fda62ecc863ee204e26
Sander-Timmerman <srt8765436@gmail.com>
2021-05-21 11:42:16 +0200

    nieuwe data

--- predict.R
+++ predict.R
@@ -12,10 +12,12 @@ gemiddelden_live <- bereken_live_gemiddelden()
 model <- train_model(gemiddelden_live)
 print(summary(model))
 
-# nieuwe_data <- combineer_data("2021", gemiddelden_live, 1)
-# voorspelling <- data.frame(Land = nieuwe_data$Land,
-#                            Bookmakers = rank(nieuwe_data$Bookmakers, ties.method = "min"),
-#                            YouTube = nrow(nieuwe_data) - rank(nieuwe_data$Youtube, ties.method = "max"),
-#                            iTunes = nrow(nieuwe_data) - rank(nieuwe_data$iTunes, ties.method = "max"),
-#                            Punten = predict(model, nieuwe_data))
-# write.xlsx(voorspelling, file = "Output/voorspelling.xlsx")
+nieuwe_data <- combineer_data("2021", gemiddelden_live, 1)
+voorspelling <- data.frame(Land = nieuwe_data$Land,
+                           Bookmakers = rank(nieuwe_data$Bookmakers, ties.method = "min"),
+                           YouTube = nrow(nieuwe_data) - rank(nieuwe_data$Youtube, ties.method = "max") + 1,
+                           iTunes = nrow(nieuwe_data) - rank(nieuwe_data$iTunes, ties.method = "max") + 1,
+                           Punten = predict(model, nieuwe_data)) %>%
+  mutate(Punten = Punten * 39 * 2 * 58 / sum(Punten)) %>%
+  arrange(desc(Punten))
+write.xlsx(voorspelling, file = "Output/voorspelling.xlsx")
